8ea9ddce8f0b0069a69ec68c5448bf0efd5c925b
Abel <abelabelao@hotmail.com.br>
2024-08-20 22:13:42 -0300

    Criação de sistema de Conta Bancária

--- contaBanco/src/ContaTerminal.java
+++ contaBanco/src/ContaTerminal.java
@@ -1,5 +1,19 @@
-public class App {
+import java.util.Scanner;
+
+public class ContaTerminal {
+
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("Por favor, digite o número da Agência com o traço");
+        String agencia = scanner.nextLine();
+        System.out.println("Por favor, digite o número da conta");
+        Integer conta = scanner.nextInt();
+        System.out.println("Informe seu nome");
+        String nome = scanner2.nextLine();
+        System.out.println("Insira o valor do saque");
+        Float saldo = scanner.nextFloat();
+
+        System.out.println("Olá " + nome +", obrigado por criar uma conta em nosso banco, sua Agência é " + agencia +", conta " + conta + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
